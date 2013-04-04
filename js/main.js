55f4888348395d397a052514294b4eb7e2e4a62f
Tom <moasth@gmail.com>
2013-04-04 23:30:35 +0200

    Added some Javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-mapbox.auto('map', 'examples.map-vyofok3q');
+$(document).ready(function(){
+
+	mapbox.auto('map', 'examples.map-vyofok3q');
+
+	$('.nav-pills a').on("click", function(e) {
+	  var $this = $(this),
+	      id = $this.attr('id'),
+	      contentId = '#' + id + '-content',
+	      $data = $(contentId).html();
+
+	  $('.content').html($data);
+
+	});
+});
+
+
