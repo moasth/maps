fc8a80fd27d89287d73228395626dc91f407f27b
Tom <moasth@gmail.com>
2013-04-06 00:46:35 +0200

    Some minor styling changes

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 	mapbox.auto('map', 'examples.map-vyofok3q');
 
 	$('.nav-pills a').on("click", function(e) {
-	  var $this = $(this),
-	      id = $this.attr('id'),
-	      contentId = '#' + id + '-content',
-	      $data = $(contentId).html();
 
-	  $('.content').html($data);
+		var  $this = $(this);
+
+		$("div.navbar ul.nav-pills li.active").removeClass("active")
+		$this.parent("li").addClass("active");
+
+	  	var     id = $this.attr('id'),
+	     contentId = '#' + id + '-content',
+	         $data = $(contentId).html();
+
+	   $('.content').html($data);
 
 	});
 });
