962491a7d6468e7be40d22c86d346b7f3edad12f
Tom <moasth@gmail.com>
2013-04-07 23:36:43 +0200

    Added maps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-	mapbox.auto('map', 'examples.map-vyofok3q');
+	//mapbox.auto('map', 'examples.map-vyofok3q');
+
+	mapbox.auto('map', ['http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt,moasth.chateaux.jsonp', 'http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt.jsonp'], function(map, details){
+		//var layers = document.getElementById('map-ui');
+		map.disableLayerAt(1);
+		//map.ui.hash.add();
+
+		map.setZoomRange(5, 12);
+		map.setPanLimits([{
+			lat: 35.3073,
+			lon: -19.6518
+		}, {
+			lat: 59.5726,
+			lon: 34.0933
+		}]);
+	});
 
 	$('.nav-pills a').on("click", function(e) {
 
@@ -9,11 +24,11 @@ $(document).ready(function(){
 		$("div.navbar ul.nav-pills li.active").removeClass("active")
 		$this.parent("li").addClass("active");
 
-	  	var     id = $this.attr('id'),
-	     contentId = '#' + id + '-content',
-	         $data = $(contentId).html();
+		var     id = $this.attr('id'),
+		contentId = '#' + id + '-content',
+		$data = $(contentId).html();
 
-	   $('.content').html($data);
+		$('.content').html($data);
 
 	});
 });
