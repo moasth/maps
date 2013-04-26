51bd82600d2dcd2abef34bf39beb27c5f219e924
Tom <moasth@gmail.com>
2013-04-26 22:19:53 +0200

    Added Wikipedia information

--- js/main.js
+++ js/main.js
@@ -2,17 +2,27 @@ $(document).ready(function(){
 
 	//mapbox.auto('map', 'examples.map-vyofok3q');
 
-	mapbox.auto('map', ['http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt,moasth.chateaux.jsonp', 'http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt.jsonp'], function(map, details){
+	//mapbox.auto('map', ['http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt,moasth.chateaux.jsonp', 'http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt.jsonp'], function(map, details){
+	mapbox.auto('map', ['http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m,moasth.map-czvq0pvt,moasth.chateaux.jsonp'], function(map, details){
 		//var layers = document.getElementById('map-ui');
 		map.disableLayerAt(1);
 		//map.ui.hash.add();
 		//map.interaction.auto().off('on').off('off').on(wax.movetip().parent(map.parent).events());
-		map.interaction.auto();
-    	map.interaction.off('on');
-    	map.interaction.off('off');
-    	map.interaction.on({
+		map.interaction.auto().on({
         	on: function(o) {
-            	$(".content > h1").html(o.data.name);
+            	var $title = $(".content > h1");
+            	
+            	if (o.data.name !== $title.html() && o.data.wikipedia) {
+					$title.html(o.data.name);
+					getWkpdData(o.data.wikipedia);
+            	} 
+            	else
+            		$title.html(o.data.name);
+            	
+                if (!o.data.wikipedia) {
+                	$(".content > p").remove();
+                	$("<p>Aucune information n'est disponible</p>").insertAfter(".content h1");
+                }
         	}
     	});
 
@@ -46,6 +56,29 @@ $(document).ready(function(){
 		$('.content').html($data);
 
 	});
+
+	function getWkpdData(t){
+            
+            $(".content").addClass("loading");
+
+            $.ajax(
+            {
+              url: "http://fr.wikipedia.org/w/api.php?format=json&action=query&titles="+t+"&prop=extracts",
+              dataType: "jsonp",
+              complete: function()
+              {
+                $(".content").removeClass("loading");
+              },
+              success: function(json)
+              {
+                var pages = json.query.pages;
+                var pageKey = Object.keys(pages)[0];
+                var $content = $(".content > p");
+                $content.html(pages[pageKey].extract);
+              }
+            });
+          }
 });
+
 
 
